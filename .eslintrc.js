e81f415dbf62e87633cdb3808b3a8228d237be3a
laspin <laspin55@gmail.com>
2022-10-16 18:12:13 +0100

    readme/update ok

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,10 @@ module.exports = {
   parser: '@babel/eslint-parser',
   parserOptions: {
     requireConfigFile: false,
+    ecmaFeatures: {
+      jsx: true,
+      sourceType: 'module',
+    },
     babelOptions: {
       presets: ['@babel/preset-react'],
     },
@@ -10,16 +14,15 @@ module.exports = {
   env: {
     browser: true,
     node: true,
-    jquery: true,
     jest: true,
   },
   rules: {
     'no-debugger': 0,
     'no-alert': 0,
     'no-await-in-loop': 0,
-    'no-return-assign': ['error', 'except-parens'],
+    'no-return-assign': ['warn', 'except-parens'],
     'no-restricted-syntax': [
-      2,
+      1,
       'ForInStatement',
       'LabeledStatement',
       'WithStatement',
@@ -32,86 +35,74 @@ module.exports = {
       },
     ],
     'prefer-const': [
-      'error',
+      'warn',
       {
         destructuring: 'all',
       },
     ],
-    'arrow-body-style': [2, 'as-needed'],
+    'arrow-body-style': ['warn', 'as-needed'],
     'no-unused-expressions': [
-      2,
+      1,
       {
         allowTaggedTemplates: true,
       },
     ],
     'no-param-reassign': [
-      2,
+      'warn',
       {
         props: false,
       },
     ],
     'no-console': 0,
     'import/prefer-default-export': 0,
-    import: 0,
     'func-names': 0,
     'space-before-function-paren': 0,
     'comma-dangle': 0,
     'max-len': 0,
     'import/extensions': 0,
     'no-underscore-dangle': 0,
+    'import/no-unresolved': 0,
+    'no-unsafe-optional-chaining': 0,
+    'react/jsx-no-useless-fragment': 0,
+    'import/no-named-as-default': 0,
+    'import/no-dynamic-require': 0,
+    'import-resolver-webpack': 0,
     'consistent-return': 0,
     'react/display-name': 1,
     'react/no-array-index-key': 0,
+    'react-hooks/rules-of-hooks': 1,
+    'react/no-unescaped-entities': 0,
+    'react-hooks/exhaustive-deps': 1,
+    'react/jsx-props-no-spreading': 0,
     'react/react-in-jsx-scope': 0,
     'react/prefer-stateless-function': 0,
     'react/forbid-prop-types': 0,
-    'react/no-unescaped-entities': 0,
-    'jsx-a11y/accessible-emoji': 0,
     'react/require-default-props': 0,
     'react/jsx-filename-extension': [
-      1,
+      'warn',
       {
         extensions: ['.js', '.jsx', '.ts', '.tsx', '.mdx'],
       },
     ],
-    radix: 0,
-    'no-shadow': [
-      2,
-      {
-        hoist: 'all',
-        allow: ['resolve', 'reject', 'done', 'next', 'err', 'error'],
-      },
-    ],
-    quotes: [
-      2,
-      'single',
-      {
-        avoidEscape: true,
-        allowTemplateLiterals: true,
-      },
-    ],
-    'prettier/prettier': [
-      'error',
-      {
-        trailingComma: 'es5',
-        singleQuote: true,
-        printWidth: 80,
-        // below line only for window users facing CLRF and eslint/prettier error
-        // non window users feel free to delete it
-        endOfLine: 'auto',
-      },
-    ],
-    'jsx-a11y/href-no-hash': 'off',
+    '@typescript-eslint/comma-dangle': 0,
+    'jsx-a11y/accessible-emoji': 0,
+    'jsx-a11y/href-no-hash': 0,
     'jsx-a11y/anchor-is-valid': [
       'warn',
       {
         aspects: ['invalidHref'],
       },
     ],
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
-    '@typescript-eslint/comma-dangle': ['off'],
-    'react/jsx-props-no-spreading': 'off',
+
+    'prettier/prettier': [
+      'warn',
+      {
+        trailingComma: 'es5',
+        singleQuote: true,
+        printWidth: 70,
+        endOfLine: 'auto',
+      },
+    ],
   },
-  plugins: ['html', 'prettier', 'react-hooks'],
+  plugins: ['react', 'react-hooks', 'prettier'],
 };
--- README.md
+++ README.md
@@ -1,1 +1,96 @@
-# eslint-config-slco
+## ESLint-Config-SLCO &nbsp; :page_with_curl:
+
+ESLint Prettier config
+
+<br />
+
+### Install Package
+
+```js
+ `npx install--peerdeps --dev eslint-config-slco`
+```
+<br />
+
+### Create a .eslintrc file and add this:
+
+```json
+ {
+    "extends": ["slco"]
+ }
+ //or inside package.json
+```
+<br />
+
+### Add linting scripts
+
+Inside package.json file:
+
+```json
+ {
+  "scripts": {
+    "lint": "eslint .",
+    "lint:fix": "eslint . --fix"
+  }
+ }
+```
+
+<br />
+
+### Using Create React App:
+
+In package.json change block that **Extends React-App** to:
+
+```json
+ {
+   "extends": "slco" 
+ }
+```
+
+
+<br />
+
+### To overwrite __eslint__ or __prettier__ settings:
+
+Do so inside of **rules** for eslint and under **prettier/prettier** for prettier
+
+```json
+  //example  
+ {
+  "extends": [
+    "slco"
+  ],
+  "rules": {
+    "no-console": 1,
+    "prettier/prettier": [
+      "warn",
+      {
+        "trailingComma": "es5",
+        "singleQuote": true,
+        "printWidth": 70,
+      }
+    ]
+  }
+ }
+```
+
+<br />
+
+### Using WebStorm
+
+```text
+- In Webstorm disable Prettier
+- Set ESLint to automatic inside Webstorm
+```
+
+
+
+
+
+
+
+
+
+
+
+
+
